1ead59bfea7955db2e214bd130d16d5d4a8ea11d
Tania Kharchuk <tkharchuk14@gmail.com>
2023-04-28 19:33:30 +0300

    Add path for logout to user/urls.py

--- social_media/settings.py
+++ social_media/settings.py
@@ -41,7 +41,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "user",
+    "rest_framework_simplejwt.token_blacklist",
 ]
 
 MIDDLEWARE = [
--- user/urls.py
+++ user/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+    TokenBlacklistView,
 )
 
 from user.views import CreateUserView
@@ -9,7 +10,8 @@ from user.views import CreateUserView
 app_name = "user"
 
 urlpatterns = [
-    path("", CreateUserView.as_view(), name="create"),
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("register/", CreateUserView.as_view(), name="create"),
+    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("logout/", TokenBlacklistView.as_view(), name="token_blacklist"),
 ]
