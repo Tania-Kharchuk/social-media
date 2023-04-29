9465b1b1362fb58a8d9d1eb5538d4ac024a2e411
Tania Kharchuk <tkharchuk14@gmail.com>
2023-04-29 11:20:52 +0300

    Add path to my-profile, my-profile/change-password and users

--- user/urls.py
+++ user/urls.py
@@ -1,17 +1,31 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
     TokenBlacklistView,
 )
 
-from user.views import CreateUserView
+from user.views import CreateUserView, UpdateUserView, UpdateProfileView, ProfileViewSet
 
+router = routers.DefaultRouter()
+router.register("", ProfileViewSet)
 app_name = "user"
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
+    path(
+        "my-profile/",
+        UpdateProfileView.as_view(),
+        name="my_profile",
+    ),
+    path(
+        "my-profile/change-password",
+        UpdateUserView.as_view(),
+        name="change-password",
+    ),
     path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("logout/", TokenBlacklistView.as_view(), name="token_blacklist"),
+    path("", include(router.urls)),
 ]
