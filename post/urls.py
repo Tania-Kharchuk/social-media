f20aefbda22d652cb1615d6cb043d014f6250cc8
Tania Kharchuk <tkharchuk14@gmail.com>
2023-05-02 14:34:58 +0300

    Add paths to comments

--- post/urls.py
+++ post/urls.py
@@ -1,14 +1,27 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from post.views import PostViewSet
+from post.views import PostViewSet, CommentViewSet
 
 router = routers.DefaultRouter()
 router.register("", PostViewSet)
+router.register("", CommentViewSet)
 app_name = "post"
 
 urlpatterns = [
     path("", include(router.urls)),
     path("<int:pk>/like/", PostViewSet.as_view({"post": "like"}), name="like"),
     path("<int:pk>/unlike/", PostViewSet.as_view({"post": "unlike"}), name="unlike"),
+    path(
+        "<int:post_id>/comment/",
+        CommentViewSet.as_view({"get": "list", "post": "create"}),
+        name="comment",
+    ),
+    path(
+        "<int:post_id>/comment/<int:pk>/",
+        CommentViewSet.as_view(
+            {"get": "retrieve", "delete": "destroy", "put": "update", "patch": "update"}
+        ),
+        name="comment-detail",
+    ),
 ]
