c397170e5c53436eaef5df3a96a631a6638cbdc1
Tania Kharchuk <tkharchuk14@gmail.com>
2023-05-02 10:48:02 +0300

    Add like and like detail serializer, add likes to post detail serializer

--- post/serializers.py
+++ post/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from post.models import Post
+from post.models import Post, Like
 from user.serializers import ProfileSerializer
 
 
@@ -18,9 +18,31 @@ class PostCreateSerializer(serializers.ModelSerializer):
         fields = ("title", "text", "media", "hashtag")
 
 
+class LikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Like
+        fields = "__all__"
+
+    def validate(self, data):
+        like = Like.objects.filter(post_id=data["post"], user_id=data["user"])
+        if like:
+            raise serializers.ValidationError("You had already liked this post")
+        return data
+
+
+class LikeDetailSerializer(serializers.ModelSerializer):
+    post = serializers.CharField(source="post.title", read_only=True)
+    user = serializers.CharField(source="user.email", read_only=True)
+
+    class Meta:
+        model = Like
+        fields = ("id", "post", "user")
+
+
 class PostDetailSerializer(serializers.ModelSerializer):
     user = ProfileSerializer(read_only=True)
+    likes = LikeDetailSerializer(many=True, read_only=True)
 
     class Meta:
         model = Post
-        fields = ("id", "title", "text", "media", "hashtag", "user")
+        fields = ("id", "title", "text", "media", "hashtag", "user", "likes")
